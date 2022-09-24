46e50c7cf2d99244f97dac61f0eb7f2946dc72ea
Simion <simion.okola@student.moringaschool.com>
2022-09-25 00:03:48 +0300

    ft-transaction form POST

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm(props) {
+
+  const transactions = props.transactions;
+  const setTransactions = props.setTransactions;
+
+  const [state, setState] = useState({});
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('http://localhost:8001/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(state)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setTransactions([
+          ...transactions,
+          data
+        ]);
+      })
+  }
+  function handleChange(evt) {
+    setState({
+      ...state,
+      [evt.target.name]: evt.target.value,
+    });
+  }
+
+
   return (
     <div className="ui segment">
       <form className="ui form">
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+function TransactionsList (props){
+
+  let componentArray = props.transactions.map(transactionObj => {
+    return <Transaction 
+            key={transactionObj.id} 
+            transaction={transactionObj} 
+          />
+  })
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,10 +27,13 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {componentArray}
       </tbody>
     </table>
   );
 }
 
 export default TransactionsList;
+
+
+
