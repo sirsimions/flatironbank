bdcd0c2ee24ef97ae57f184dcb0a39194ea33f6e
Simion <simion.okola@student.moringaschool.com>
2022-09-25 14:55:32 +0300

    ft-debugging search

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ const Search = (props) =>{
       <input
         type="text"
         placeholder="Search your Recent Transactions"
-        onChange={(handleChange) => console.log("Searching...")}
+        onChange={(handleChange) /*=> console.log("Searching...")*/}
       />
       <i className="circular search link icon"></i>
     </div>
