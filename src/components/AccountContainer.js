070a3cd07fde311a0f48e30b54f61b45f0d0f9cd
Simion <simion.okola@student.moringaschool.com>
2022-09-25 11:41:01 +0300

    ft - account container useState

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,6 +4,8 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
+  const [search, setSearch] = useState('');
+  const [transactions, setTransactions] = useState([]);
   return (
     <div>
       <Search />
