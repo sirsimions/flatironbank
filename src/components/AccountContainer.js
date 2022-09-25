ea07c13c6620eef66fb6ce1ba488420b53b7e28f
Simion <simion.okola@student.moringaschool.com>
2022-09-25 14:53:09 +0300

    ft-debugging account container

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState} from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
